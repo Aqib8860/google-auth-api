ad079bd6bf61a5f562013a8e472bf04fcc32dd51
Aditya Pal <adityapal2000.2013@gmail.com>
2021-06-04 17:22:54 +0530

    id to string

--- server/views.py
+++ server/views.py
@@ -7,9 +7,6 @@ from utils import db
 
 async def hello(request):
     # raise Exception("Custom Exception")
-    with db.Connect() as client:
-        profiles = client.auth.profile.delete_many({})
-        print(profiles)
 
 
     # await settings.groups.group_send("followers", data={"async message received": "Noice work aditya", "type": "websocket.accept"})
