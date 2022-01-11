bf30065de7d4927c2ae81d180f307ece99456240
Ubuntu <ubuntu@ip-172-31-37-161.ap-south-1.compute.internal>
2022-01-11 13:48:07 +0000

    11Jan

--- server/settings.py
+++ server/settings.py
@@ -22,7 +22,25 @@ MIDDLEWARE = [
 
 #app = Starlette(routes=routes, debug=SERVER.DEBUG, middleware=MIDDLEWARE,)
 
+
+"""class Groups:
+    def __init__(self):
+        self.groups = {}
+
+    def group_add(self, group_name, websocket_endpoint):
+        if group := self.groups.get(group_name):
+
+            group.append(websocket_endpoint)
+        else:
+            self.groups.update({group_name: [websocket_endpoint, ]})
+
+    def group_discard(self, group_name, websocket_endpoint):
+        if group_name in self.groups:
+            self.groups[group_name].remove(websocket_endpoint)
+
+    async def group_send(self, group_name, data):
+        if group := self.groups.get(group_name):
+            for i in group:
+                await i.broadcast(data=data)"""
+                        
 groups = Groups()
-
-
-
