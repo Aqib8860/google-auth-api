997b5933834ee2faa1a22b92ea463ff1799570ac
Aqib Khan <67043041+Aqib8860@users.noreply.github.com>
2022-01-17 12:46:13 +0530

    Update local_settings.py

--- server/local_settings.py
+++ server/local_settings.py
@@ -1,8 +1,8 @@
 import datetime
 
 class DBInfo(object):
-    MONGO_USER = "myworld"
-    MONGO_PASSWORD = "hgrhtAAeybVuuHCc"
+    MONGO_USER = ""
+    MONGO_PASSWORD = ""
 
     @staticmethod
     def get_url():
@@ -10,20 +10,20 @@ class DBInfo(object):
         #return f"mongodb+srv://{DBInfo.MONGO_USER}:{DBInfo.MONGO_PASSWORD}@cluster0.jzv7p.mongodb.net/myworld?authSource=admin&replicaSet=atlas-39hn0j-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
  
 class AWS:
-    ACCESS_KEY = 'AKIASGPFZLBMQV5ACHCL'
-    SECRET_ACCESS_KEY = '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'
-    REGION_NAME = 'ap-south-1'
+    ACCESS_KEY = ''
+    SECRET_ACCESS_KEY = ''
+    REGION_NAME = ''
 
     # AWS BUCKET
-    STORAGE_BUCKET_NAME = 'new-myworld-bucket'
+    STORAGE_BUCKET_NAME = ''
     DEFAULT_ACL = 'public-read'
 
     # AWS SNS 
-    PLATFORM_APPLICATION = "arn:aws:sns:ap-south-1:151342635097:app/GCM/Cessini"
+    PLATFORM_APPLICATION = ""
 
 class FIREBASE:
-    CERTIFICATE = 'firebase.json'
-    DATABASE_URL = 'https://myworld-311307-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    CERTIFICATE = ''
+    DATABASE_URL = ''
 
     
     
@@ -50,10 +50,10 @@ class SECURITY:
 
 class SOCIAL:
     GOOGLE_CLIENT_ID =  [
-        '909136434512-cca9i6u7cjo19l8583l6ho5q6c5h7ccf.apps.googleusercontent.com',  # Android
-        '1047620411446-dltsdc36bvb5ui0883m55emk0id55350.apps.googleusercontent.com'  # Web
+        '',  # Android
+        ''  # Web
     ]
-    SOCIAL_SECRET = 'E9JOBS3i-JPX2o1wZs-9cQ6F'
+    SOCIAL_SECRET = ''
 
-    FACEBOOK_APP_ID = '800430383906982'
-    FACEBOOK_SECRET = "119276e4178d7754d0abf81d3e946ad5"
+    FACEBOOK_APP_ID = ''
+    FACEBOOK_SECRET = ""
